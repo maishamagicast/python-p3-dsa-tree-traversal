56c7370251c708903c639305647bde20775a526e
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-07-08 12:04:45 +0300

    binary tress done

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,17 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit=[self.root]
+    while nodes_to_visit:
+      node=nodes_to_visit.pop()
+      if node['id']==id:
+        return node
+      nodes_to_visit.extend(node.get('children', []))
+    return None
+      
+# {
+#   'tag_name': 'h1',
+#   'id': 'heading',
+#   'text_content': 'Title',
+#   'children': []
+# }
